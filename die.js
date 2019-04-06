28f12443e4323d9dc20a84348214546558c9e41a
h22121 <h22121@lab.cpcc.edu>
2019-04-06 12:55:12 -0400

    classwork1255pm

--- die.js
+++ die.js
@@ -9,19 +9,19 @@ class Die {
         return 6;
     }
 
-    set faces(dieFaces)  {
-        if(Array.isArray(dieFaces)) {                      //validate it is an array
-            for(var i = 0;  i < dieFaces.length; i++) {    //use < len  OR  <=Len-1
-                if(!(dieFaces[i] instanceof Face))  {  // validate that every part is what i need
-                    dieFaces[i] = new Face(i,i);
+    set faces(dieFaces) {
+        if (Array.isArray(dieFaces)) {                      //validate it is an array
+            for (var i = 0; i < dieFaces.length; i++) {    //use < len  OR  <=Len-1
+                if (!(dieFaces[i] instanceof Face)) {  // validate that every part is what i need
+                    dieFaces[i] = new Face(i, i);
                 }
             }
         }
         else {
             var dieFaces = [];
 
-            for(var i = 0; i < Die.defaultNumberOfFaces(); i++) {
-                dieFaces.push(new Face(i,i + 1,));
+            for (var i = 0; i < Die.defaultNumberOfFaces(); i++) {
+                dieFaces.push(new Face(i, i + 1));
             }
         }
         this._faces = dieFaces;      //assign die faces to the faces value
@@ -36,12 +36,12 @@ class Die {
 
         var upper_bound = this._faces.length - 1;
         var lower_bound = 0;
-        var randomFaceIndex = Math.floor(Math.random()*(upper_bound - lower_bound) + lower_bound);
+        var randomFaceIndex = Math.floor(Math.random() * (upper_bound - lower_bound) + lower_bound);
 
         this._activeFaceIndex = randomaceIndex;
     }
 
-    get ActiveFace()   {
+    get ActiveFace() {
         return this._faces[this._activeFaceIndex];
     }
 
